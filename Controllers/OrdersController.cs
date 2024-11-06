9b3034dd2f5f05bbdb02d2d51e50803cc8cea526
Saleem Kassab <saleemkassab5@gmail.com>
2024-11-06 15:16:24 +0300

    update GetOrders

--- Controllers/OrdersController.cs
+++ Controllers/OrdersController.cs
@@ -9,319 +9,356 @@ using SyriatelCatering.API.Models.DTOs;
 
 namespace Syriatel_Cafe.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    [Authorize]
-    public class OrdersController : ControllerBase
-    {
-        //private CafeModel db = new CafeModel();
-        private readonly CafeModel db;
-        public OrdersController(CafeModel cafeModel)
-        {
-            db = cafeModel;
-            timeZone = TimeZoneInfo.FindSystemTimeZoneById("Arab Standard Time");
-        }
-        private TimeZoneInfo timeZone;
-        // GET: api/Orders
-        [Authorize(Roles = "MIS-Technical Data Analysis,AD-Catering Cashier,AD-Catering Staff,AD-Catering HOSs")]
-        [HttpPost("getorders")]
-        public IActionResult GetOrders([FromBody] GetOrderFilter filter)
-        {
-            filter.Date = TimeZoneInfo.ConvertTimeFromUtc(filter.Date, timeZone);
-            var result = db.Orders.Include(x => x.Status).Include(x => x.User).Where(x => !x.IsDeleted && x.CreateDate >= filter.Date && x.CreateDate < filter.Date.AddDays(1)).ToList();
-            return Ok(result);
-        }
-
-        // GET: api/Orders/5
-        //[ResponseType(typeof(Order))]
-        [HttpGet("{id}")]
-        public IActionResult GetOrder(int id)
-        {
-            var order = db.Orders.Include(x => x.Transactions).Where(x => x.Id == id).ToList();
-            if (order == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(order);
-        }
-
-        // PUT: api/Orders/5
-        //[ResponseType(typeof(void))]
-        [HttpPut]
-        public IActionResult PutOrder(int id, Order order)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            if (id != order.Id)
-            {
-                return BadRequest();
-            }
-
-            db.Entry(order).State = EntityState.Modified;
-
-            try
-            {
-                db.SaveChanges();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!OrderExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            //return StatusCode(HttpStatusCode.NoContent);
-            return NotFound();
-        }
-
-        // POST: api/Orders
-        //[ResponseType(typeof(Order))]
-        [HttpPost]
-        public IActionResult PostOrder(Order order)
-        {
-            //WindowsIdentity identity = HttpContext.Current.Request.LogonUserIdentity;
-            var identity = User.Identity.Name;
-
-
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            List<string> checkResult = CheckAvailable(order);
-
-            if (checkResult.Count == 0)
-            {
-
-                foreach (var t in order.Transactions)
-
-                {
-
-                    var lstExtra = t.Extras.ToList();
-                    t.Extras.Clear();
-
-
-                    foreach (var e in lstExtra)
-                    {
-                        t.Extras.Add(db.Extras.Find(e.Id));
+	[Route("api/[controller]")]
+	[ApiController]
+	[Authorize]
+	public class OrdersController : ControllerBase
+	{
+		//private CafeModel db = new CafeModel();
+		private readonly CafeModel db;
+		public OrdersController(CafeModel cafeModel)
+		{
+			db = cafeModel;
+			timeZone = TimeZoneInfo.FindSystemTimeZoneById("Arab Standard Time");
+		}
+		private TimeZoneInfo timeZone;
+		// GET: api/Orders
+		[Authorize(Roles = "MIS-Technical Data Analysis,AD-Catering Cashier,AD-Catering Staff,AD-Catering HOSs")]
+		[HttpPost("getorders")]
+		public IActionResult GetOrders([FromBody] GetOrderFilter filter)
+		{
+			filter.Date = TimeZoneInfo.ConvertTimeFromUtc(filter.Date, timeZone);
+			//var result = db.Orders.Include(x => x.Status).Include(x => x.User).Where(x => !x.IsDeleted && x.CreateDate >= filter.Date && x.CreateDate < filter.Date.AddDays(1)).ToList();
+
+			var result = db.Orders
+				.Where(e => !e.IsDeleted && e.CreateDate >= filter.Date && e.CreateDate < filter.Date.AddDays(1))
+				.Select(e => new Order
+				{
+					Id = e.Id,
+					CreateDate = e.CreateDate,
+					IsDeleted = e.IsDeleted,
+					Location = e.Location,
+					Note = e.Note,
+					Roof = e.Roof,
+					StatusId = e.StatusId,
+					Total = e.Total,
+					UserName = e.UserName,
+
+					Status = new Status
+					{
+						Id = e.Status.Id,
+						IsDeleted = e.Status.IsDeleted,
+						Description = e.Status.Description,
+						Name = e.Status.Name,
+						NextStatus = e.Status.NextStatus,
+						NextStatusId = e.Status.NextStatusId
+					},
+					User = new User
+					{
+						UserName = e.UserName,
+						CreateDate = e.User.CreateDate,
+						Floor = e.User.Floor,
+						Note = e.User.Note,
+						Location = e.User.Location,
+						Phone = e.User.Phone
+					}
+
+				})
+				.ToList();
+
+			return Ok(result);
+		}
+
+		// GET: api/Orders/5
+		//[ResponseType(typeof(Order))]
+		[HttpGet("{id}")]
+		public IActionResult GetOrder(int id)
+		{
+			var order = db.Orders.Include(x => x.Transactions).Where(x => x.Id == id).ToList();
+			if (order == null)
+			{
+				return NotFound();
+			}
+
+			return Ok(order);
+		}
+
+		// PUT: api/Orders/5
+		//[ResponseType(typeof(void))]
+		[HttpPut]
+		public IActionResult PutOrder(int id, Order order)
+		{
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(ModelState);
+			}
+
+			if (id != order.Id)
+			{
+				return BadRequest();
+			}
+
+			db.Entry(order).State = EntityState.Modified;
+
+			try
+			{
+				db.SaveChanges();
+			}
+			catch (DbUpdateConcurrencyException)
+			{
+				if (!OrderExists(id))
+				{
+					return NotFound();
+				}
+				else
+				{
+					throw;
+				}
+			}
 
-                    }
-                    var product = db.Products.Find(t.ProductId);
+			//return StatusCode(HttpStatusCode.NoContent);
+			return NotFound();
+		}
 
-                    var Aquantity = product.AvailableProduact;
+		// POST: api/Orders
+		//[ResponseType(typeof(Order))]
+		[HttpPost]
+		public IActionResult PostOrder(Order order)
+		{
+			//WindowsIdentity identity = HttpContext.Current.Request.LogonUserIdentity;
+			var identity = User.Identity.Name;
 
 
-                    Aquantity = Aquantity - t.Quantity;
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(ModelState);
+			}
 
+			List<string> checkResult = CheckAvailable(order);
 
-                    product.AvailableProduact = Aquantity;
+			if (checkResult.Count == 0)
+			{
 
-                    db.Entry(product).State = EntityState.Modified;
+				foreach (var t in order.Transactions)
 
-                    db.SaveChanges();
+				{
 
+					var lstExtra = t.Extras.ToList();
+					t.Extras.Clear();
 
-                }
 
-                //order.UserName = identity.Name.Substring(9);
-                order.UserName = identity.Substring(9);
+					foreach (var e in lstExtra)
+					{
+						t.Extras.Add(db.Extras.Find(e.Id));
 
-                db.Orders.Add(order);
-                db.SaveChanges();
+					}
+					var product = db.Products.Find(t.ProductId);
 
-                return Ok(order);
-            }
-            else
-            {
-                return Ok(checkResult);
-            }
-        }
+					var Aquantity = product.AvailableProduact;
 
 
-        // DELETE: api/Orders/5
-        //[ResponseType(typeof(Order))]
-        [HttpDelete]
-        public IActionResult DeleteOrder(int id)
-        {
-            Order order = db.Orders.Find(id);
-            if (order == null)
-            {
-                return NotFound();
-            }
+					Aquantity = Aquantity - t.Quantity;
 
-            db.Orders.Remove(order);
-            db.SaveChanges();
 
-            return Ok(order);
-        }
+					product.AvailableProduact = Aquantity;
 
-        //protected override void Dispose(bool disposing)
-        //{
-        //    if (disposing)
-        //    {
-        //        db.Dispose();
-        //    }
-        //    base.Dispose(disposing);
-        //}
+					db.Entry(product).State = EntityState.Modified;
 
-        private bool OrderExists(int id)
-        {
-            return db.Orders.Count(e => e.Id == id) > 0;
-        }
+					db.SaveChanges();
 
 
-        [HttpGet]
-        [Authorize(Roles = "MIS-Technical Data Analysis,AD-Catering Cashier,AD-Catering Staff,AD-Catering HOSs")]
-        [Route("/api/HandelOrder")]
-        public IActionResult HandelTicket(int orderid)
-        {
-            Order order = db.Orders.Find(orderid);
+				}
 
-            var nextStatus = db.Status.Where(x => x.Id == order.StatusId).Select(x => x.NextStatusId).Single();
+				//order.UserName = identity.Name.Substring(9);
+				order.UserName = identity.Substring(9);
 
-            if (nextStatus != null)
-            {
-                order.StatusId = Convert.ToInt32(nextStatus);
+				db.Orders.Add(order);
+				db.SaveChanges();
 
-                db.Entry(order).State = EntityState.Modified;
+				return Ok(order);
+			}
+			else
+			{
+				return Ok(checkResult);
+			}
+		}
 
-                db.SaveChanges();
 
-                return Ok(true);
+		// DELETE: api/Orders/5
+		//[ResponseType(typeof(Order))]
+		[HttpDelete]
+		public IActionResult DeleteOrder(int id)
+		{
+			Order order = db.Orders.Find(id);
+			if (order == null)
+			{
+				return NotFound();
+			}
 
-            }
+			db.Orders.Remove(order);
+			db.SaveChanges();
 
-            else
-            {
-                return Ok(false);
-            }
+			return Ok(order);
+		}
 
+		//protected override void Dispose(bool disposing)
+		//{
+		//    if (disposing)
+		//    {
+		//        db.Dispose();
+		//    }
+		//    base.Dispose(disposing);
+		//}
 
+		private bool OrderExists(int id)
+		{
+			return db.Orders.Count(e => e.Id == id) > 0;
+		}
 
 
-        }
+		[HttpGet]
+		[Authorize(Roles = "MIS-Technical Data Analysis,AD-Catering Cashier,AD-Catering Staff,AD-Catering HOSs")]
+		[Route("/api/HandelOrder")]
+		public IActionResult HandelTicket(int orderid)
+		{
+			Order order = db.Orders.Find(orderid);
 
-        [HttpGet]
-        [Authorize(Roles = "MIS-Technical Data Analysis,AD-Catering Cashier,AD-Catering Staff,AD-Catering HOSs")]
-        [Route("/api/RejectOrder")]
-        public IActionResult RejectOrder(int orderid)
-        {
-            var order = db.Orders.Include(x => x.Transactions).FirstOrDefault(y => y.Id == orderid);
+			var nextStatus = db.Status.Where(x => x.Id == order.StatusId).Select(x => x.NextStatusId).Single();
 
+			if (nextStatus != null)
+			{
+				order.StatusId = Convert.ToInt32(nextStatus);
 
-            if (order != null)
-            {
-                order.StatusId = 4;
-                db.Entry(order).State = EntityState.Modified;
+				db.Entry(order).State = EntityState.Modified;
 
-                db.SaveChanges();
+				db.SaveChanges();
 
+				return Ok(true);
 
-                foreach (var s in order.Transactions)
-                {
+			}
 
-                    var product = db.Products.Find(s.ProductId);
+			else
+			{
+				return Ok(false);
+			}
 
-                    int reloadProductAvailable = product.AvailableProduact + s.Quantity;
 
-                    product.AvailableProduact = reloadProductAvailable;
 
-                    db.Entry(product).State = EntityState.Modified;
 
-                    db.SaveChanges();
+		}
 
-                }
+		[HttpGet]
+		[Authorize(Roles = "MIS-Technical Data Analysis,AD-Catering Cashier,AD-Catering Staff,AD-Catering HOSs")]
+		[Route("/api/RejectOrder")]
+		public IActionResult RejectOrder(int orderid)
+		{
+			var order = db.Orders.Include(x => x.Transactions).FirstOrDefault(y => y.Id == orderid);
 
-                return Ok(true);
 
-            }
-            else
-            {
-                return Ok(false);
-            }
+			if (order != null)
+			{
+				order.StatusId = 4;
+				db.Entry(order).State = EntityState.Modified;
 
+				db.SaveChanges();
 
-        }
 
+				foreach (var s in order.Transactions)
+				{
 
-        private List<string> CheckAvailable(Order t)
-        {
-            List<string> NoAvailable = new List<string>();
+					var product = db.Products.Find(s.ProductId);
 
-            string msg = "";
+					int reloadProductAvailable = product.AvailableProduact + s.Quantity;
 
-            foreach (Transaction s in t.Transactions)
-            {
+					product.AvailableProduact = reloadProductAvailable;
 
-                var product = db.Products.Find(s.ProductId);
+					db.Entry(product).State = EntityState.Modified;
 
-                var Aquantity = product.AvailableProduact;
+					db.SaveChanges();
 
+				}
 
-                if (Convert.ToInt32(Aquantity) < s.Quantity)
-                {
-                    msg = product.Name;
+				return Ok(true);
 
-                    NoAvailable.Add(msg);
+			}
+			else
+			{
+				return Ok(false);
+			}
 
-                }
 
+		}
 
-            }
 
-            return NoAvailable;
+		private List<string> CheckAvailable(Order t)
+		{
+			List<string> NoAvailable = new List<string>();
 
+			string msg = "";
 
+			foreach (Transaction s in t.Transactions)
+			{
 
+				var product = db.Products.Find(s.ProductId);
 
-        }
+				var Aquantity = product.AvailableProduact;
 
 
-        [HttpGet]
-        [Route("/api/OrderUser")]
-        public IActionResult GetUserOrder([FromQuery]DateTime dateFilter)
-        {
-            //WindowsIdentity identity = HttpContext.Current.Request.LogonUserIdentity;
-            var identity = User.Identity.Name;
+				if (Convert.ToInt32(Aquantity) < s.Quantity)
+				{
+					msg = product.Name;
 
+					NoAvailable.Add(msg);
 
-            //string d = db.Orders.Select(x => EntityFunctions.TruncateTime(x.CreateDate)).ToString();
+				}
 
-            ////var orders = db.Orders.Where(x => x.UserName == identity.Name.Substring(9) && EntityFunctions.TruncateTime(x.CreateDate) == EntityFunctions.TruncateTime(dateFilter)).Include(x => x.Status);
-            //var orders = db.Orders.Where(x => x.UserName == identity.Substring(9) && EntityFunctions.TruncateTime(x.CreateDate) == EntityFunctions.TruncateTime(dateFilter)).Include(x => x.Status);
 
-            //return Ok(orders);
+			}
 
-            dateFilter = TimeZoneInfo.ConvertTimeFromUtc(dateFilter, timeZone);
-            var result = db.Orders.Include(x => x.Status).Where(x => x.UserName == identity.Substring(9) && !x.IsDeleted && x.CreateDate >= dateFilter && x.CreateDate < dateFilter.AddDays(1)).ToList();
-            return Ok(result);
+			return NoAvailable;
 
 
 
-        }
 
-        [HttpGet]
-        [Route("/api/orderTransaction")]
-        public IActionResult GetTransaction(int orderid)
-        {
-            //WindowsIdentity identity = HttpContext.Current.Request.LogonUserIdentity;
-            var identity = User.Identity.Name;
+		}
 
 
-            var transaction = db.Transactions.Where(x => x.OrdertId == orderid).Include(y => y.Product).Include(z => z.Extras);
+		[HttpGet]
+		[Route("/api/OrderUser")]
+		public IActionResult GetUserOrder([FromQuery] DateTime dateFilter)
+		{
+			//WindowsIdentity identity = HttpContext.Current.Request.LogonUserIdentity;
+			var identity = User.Identity.Name;
 
-            return Ok(transaction);
 
+			//string d = db.Orders.Select(x => EntityFunctions.TruncateTime(x.CreateDate)).ToString();
 
-        }
-    }
+			////var orders = db.Orders.Where(x => x.UserName == identity.Name.Substring(9) && EntityFunctions.TruncateTime(x.CreateDate) == EntityFunctions.TruncateTime(dateFilter)).Include(x => x.Status);
+			//var orders = db.Orders.Where(x => x.UserName == identity.Substring(9) && EntityFunctions.TruncateTime(x.CreateDate) == EntityFunctions.TruncateTime(dateFilter)).Include(x => x.Status);
+
+			//return Ok(orders);
+
+			dateFilter = TimeZoneInfo.ConvertTimeFromUtc(dateFilter, timeZone);
+			var result = db.Orders.Include(x => x.Status).Where(x => x.UserName == identity.Substring(9) && !x.IsDeleted && x.CreateDate >= dateFilter && x.CreateDate < dateFilter.AddDays(1)).ToList();
+			return Ok(result);
+
+
+
+		}
+
+		[HttpGet]
+		[Route("/api/orderTransaction")]
+		public IActionResult GetTransaction(int orderid)
+		{
+			//WindowsIdentity identity = HttpContext.Current.Request.LogonUserIdentity;
+			var identity = User.Identity.Name;
+
+
+			var transaction = db.Transactions.Where(x => x.OrdertId == orderid).Include(y => y.Product).Include(z => z.Extras);
+
+			return Ok(transaction);
+
+
+		}
+	}
 }
